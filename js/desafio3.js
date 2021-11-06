870e4d2ef2bed35703e4146011f187cdf8f01887
Natali Blussand <nataliblussand@gmail.com>
2021-11-06 18:27:25 -0300

    desafio 3

---
+++ js/desafio3.js
@@ -0,0 +1,9 @@
+let Instrucciones = prompt("Ingrese un saludo!");
+
+
+while(Instrucciones != "ESC"){
+    alert("Bienvenido/as! Estamos felices de que visiten FAVISA!");
+    Instrucciones = prompt("Para ver nuestro catálogo dirijase a la pestaña PRODUCTOS. \nPresione ACEPTAR para más tips o escriba ESC para salir!");
+    Instrucciones = prompt("También podes hacer tu compra por WhatsApp! Tenemos un catálogo con los productos más vendidos!. \nPresione ACEPTAR para más tips o escriba ESC para salir!");
+    Instrucciones = prompt("Los pedidos se procesan y se entregan en nuestro horario de atención: de Lunes a Viernes de 9 a 13 hs y de 14 a 18 hs. \nMuchas gracias por su interés!. \nEscriba ESC para salir!");
+}
